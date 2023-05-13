607c717e19a57314a9a10856ae0b7b5059615540
Anselm Schüler <mail@anselmschueler.com>
2023-05-13 15:15:42 +0200

    Version 2.0.0

--- callableabc/callableabc.py
+++ callableabc/callableabc.py
@@ -7,17 +7,14 @@ def make_callable_abc_meta(class_call_name: str, /) -> type:
             self, name: str, bases: tuple[type, ...], namespace: dict[str, object]
         ) -> None:
             if class_call_name not in namespace:
-                raise TypeError(
-                    f"can't create CallableABC({class_call_name!r}) class without a {class_call_name} method"
-                )
+                namespace[class_call_name] = None
             super().__init__(name, bases, namespace)
 
-        __init__.qualname = "CallableABCMeta.__init__"  # type: ignore
-
         def __call__(self, *args: object, **kwargs: object) -> object:
-            return getattr(self, class_call_name)(*args, **kwargs)
-
-        __call__.qualname = "CallableABCMeta.__call__"  # type: ignore
+            class_call = getattr(self, class_call_name, None)
+            if class_call is not None and callable(class_call):
+                return getattr(self, class_call_name)(*args, **kwargs)
+            return super().__call__(*args, **kwargs)
 
     CallableABCMeta.__qualname__ = "CallableABCMeta"
     return CallableABCMeta
@@ -28,6 +25,7 @@ def make_callable_abc(class_call_name: str = "class_call", /) -> type:
         "CallableABC", (), {class_call_name: staticmethod(make_callable_abc)}
     )
     callable_abc.__qualname__ = "CallableABC"
+    callable_abc.__module__ = "callableabc.callableabc"
     return callable_abc
 
 
